3f27f850825443bfdfee43d94152c62a9f0189f5
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-12 12:59:26 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
 def my_each # put argument(s) here
-  # code here
+  def my_each(arr)
+     i = 0
+end 	  while i < arr.length
+    yield(arr[i])
+    i += 1
+  end
+  arr
+end
+
+# arr = [1,2,3,4]
+
+# my_each(arr) do |i|
+#   puts "#{i}"
+# end 
+
 end
